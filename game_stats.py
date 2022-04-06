3957d3d4d4e521a166b498b46727363379ab01fd
Yurii <yra.pilipsv@gmail.com>
2022-04-06 21:50:32 +0300

    Game stats class added. also added new relationship between the ship and aliens

--- game_stats.py
+++ game_stats.py
@@ -3,6 +3,8 @@ class GameStats:
 
     def __init__(self, ai_game):
         """Statistic initialization"""
+        self.dead_aliens = None
+        self.ships_left = None
         self.settings = ai_game.settings
         self.reset_stats()
 
